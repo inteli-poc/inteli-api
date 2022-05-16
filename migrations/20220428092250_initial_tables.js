4b4ccb97f0769e8be6f108b5a51a65466104289d
Mark Addis <nitro_charlie@live.com>
2022-05-16 11:17:26 +0100

    Move orders to own migration

--- migrations/20220428092250_initial_tables.js
+++ migrations/20220428092250_initial_tables.js
@@ -18,15 +18,6 @@ exports.up = async (knex) => {
     def.primary(['id'])
   })
 
-  await knex.schema.createTable('orders', (def) => {
-    def.uuid('id').defaultTo(uuidGenerateV4())
-    def.string('supplier', 255).notNullable()
-    def.specificType('items', 'uuid Array').notNullable()
-    def.datetime('required_by').notNullable()
-    def.datetime('created_at').notNullable().default(now())
-    def.datetime('updated_at').notNullable().default(now())
-  })
-
   await knex.schema.createTable('recipes', (def) => {
     def.uuid('id').defaultTo(uuidGenerateV4())
 
@@ -50,6 +41,5 @@ exports.up = async (knex) => {
 exports.down = async (knex) => {
   await knex.schema.dropTable('recipes')
   await knex.schema.dropTable('attachments')
-  await knex.schema.dropTable('orders')
   await knex.raw('DROP EXTENSION "uuid-ossp"')
 }
---
+++ migrations/20220516100946_orders.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  const [extInstalled] = await knex('pg_extension').select('*').where({ extname: 'uuid-ossp' })
+
+  if (!extInstalled) {
+    await knex.raw('CREATE EXTENSION "uuid-ossp"')
+  }
+
+  const uuidGenerateV4 = () => knex.raw('uuid_generate_v4()')
+  const now = () => knex.fn.now()
+
+  await knex.schema.createTable('orders', (def) => {
+    def.uuid('id').defaultTo(uuidGenerateV4())
+    def.string('supplier', 255).notNullable()
+    def.specificType('items', 'uuid Array').notNullable()
+    def.datetime('required_by').notNullable()
+    def.datetime('created_at').notNullable().default(now())
+    def.datetime('updated_at').notNullable().default(now())
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.dropTable('orders')
+  await knex.raw('DROP EXTENSION "uuid-ossp"')
+}
