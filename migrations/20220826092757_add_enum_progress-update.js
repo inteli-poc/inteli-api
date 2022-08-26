11b959419122ba51b2e75f6c9f6f6154657af2f6
athilkabbas123 <a.kuzhikkandathil@accenture.com>
2022-08-26 14:23:11 +0100

    running eslint
    Signed-off-by: athilkabbas123 <a.kuzhikkandathil@accenture.com>

--- migrations/20220826092757_add_enum_progress-update.js
+++ migrations/20220826092757_add_enum_progress-update.js
@@ -2,33 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up = async function (knex) {
-    await knex.schema.alterTable('build_transactions', (def) => {
-      def.dropColumn('type')
-    })
-    await knex.schema.alterTable('build_transactions',(def) => {
-        def
-        .enu('type', ['Schedule', 'Start','progress-update' ,'Submit'], {
-          useNative: true
-        })
-        .notNullable()
-    })
-  }
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  exports.down = async function (knex) {
-    await knex.schema.alterTable('build_transactions', (def) => {
-        def.dropColumn('type')
+exports.up = async function (knex) {
+  await knex.schema.alterTable('build_transactions', (def) => {
+    def.dropColumn('type')
+  })
+  await knex.schema.alterTable('build_transactions', (def) => {
+    def
+      .enu('type', ['Schedule', 'Start', 'progress-update', 'Submit'], {
+        useNative: true,
       })
-      await knex.schema.alterTable('build_transactions',(def) => {
-        def
-        .enu('type', ['Schedule', 'Start' ,'Submit'], {
-          useNative: true
-        })
-        .notNullable()
-    })
-  }
-  
+      .notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.alterTable('build_transactions', (def) => {
+    def.dropColumn('type')
+  })
+  await knex.schema.alterTable('build_transactions', (def) => {
+    def
+      .enu('type', ['Schedule', 'Start', 'Submit'], {
+        useNative: true,
+      })
+      .notNullable()
+  })
+}
