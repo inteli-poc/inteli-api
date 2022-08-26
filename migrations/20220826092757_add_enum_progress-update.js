ccd826676d1af9329fe13c023c6f51a17426e925
athilkabbas123 <a.kuzhikkandathil@accenture.com>
2022-08-26 14:22:12 +0100

    fixing build transaction column type
    Signed-off-by: athilkabbas123 <a.kuzhikkandathil@accenture.com>

---
+++ migrations/20220826092757_add_enum_progress-update.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = async function (knex) {
+    await knex.schema.alterTable('build_transactions', (def) => {
+      def.dropColumn('type')
+    })
+    await knex.schema.alterTable('build_transactions',(def) => {
+        def
+        .enu('type', ['Schedule', 'Start','progress-update' ,'Submit'], {
+          useNative: true
+        })
+        .notNullable()
+    })
+  }
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = async function (knex) {
+    await knex.schema.alterTable('build_transactions', (def) => {
+        def.dropColumn('type')
+      })
+      await knex.schema.alterTable('build_transactions',(def) => {
+        def
+        .enu('type', ['Schedule', 'Start' ,'Submit'], {
+          useNative: true
+        })
+        .notNullable()
+    })
+  }
+  
