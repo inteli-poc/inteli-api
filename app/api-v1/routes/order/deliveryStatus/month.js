1ad371de4fe1d9ab20b0711285e18df7d61d6feb
Ampolu <shailesh.ampolu@accenture.com>
2025-01-21 09:40:00 +0000

    Adding delivery status by month file

---
+++ app/api-v1/routes/order/deliveryStatus/month.js
@@ -0,0 +1,39 @@
+const { getDefaultSecurity } = require('../../../../utils/auth');
+const order = require('../../../controllers/Order');
+const { buildValidatedJsonHandler } = require('../../../../utils/routeResponseValidator');
+
+module.exports = function () {
+  const doc = {
+    GET: buildValidatedJsonHandler(order.getDeliveryStatus('month'), {
+      summary: 'List Purchase Order Status for past 6 months',
+      description: 'Returns the details of all on-chain orders from the past 6 months.',
+      parameters: [],  
+      responses: {
+        200: {
+          description: 'Return Purchase Order delivery status',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/orderDeliveryStatus', 
+              },
+            },
+          },
+        },
+        404: {
+          description: 'Order Status not found',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/NotFoundError',
+              },
+            },
+          },
+        },
+      },
+      security: getDefaultSecurity(), 
+      tags: ['order'], 
+    }),
+  };
+
+  return doc;
+};
