bd22fadf5748d14904cc44a3b21ae1e9a32e8168
shaileshAsk <shailesh.ampolu@accenture.com>
2025-03-20 18:25:52 +0000

    Alter Build transactions

--- migrations/20250320163453_update_build_transaction_types.js
+++ migrations/20250320163453_update_build_transaction_types.js
@@ -3,21 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-    await knex.schema.raw(`
-        DO $$ 
-        BEGIN
-            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'build_transaction_type') THEN
-                CREATE TYPE build_transaction_type AS ENUM ('Schedule', 'Start', 'progress-update', 'Complete');
-            END IF;
-        END $$;
-        
-        ALTER TYPE build_transaction_type ADD VALUE IF NOT EXISTS 'Simulation';
-        ALTER TYPE build_transaction_type ADD VALUE IF NOT EXISTS 'Approval';
-        ALTER TYPE build_transaction_type ADD VALUE IF NOT EXISTS 'Created';
-
-        ALTER TABLE build_transactions 
-        ALTER COLUMN type TYPE build_transaction_type USING type::text::build_transaction_type;
-    `);
+    await knex.schema.alterTable("build_transactions", (table) => {
+        table.enu("type", [
+            "Schedule",
+            "Start",
+            "progress-update",
+            "Complete",
+            "Simulation",  
+            "Approval",    
+            "Created"      
+        ]).notNullable().alter();
+    });
 };
 
 /**
