4ba5c36822ccc16523ed453bf67eca3551361ee3
shaileshAsk <shailesh.ampolu@accenture.com>
2025-03-21 14:06:40 +0000

    Syntax correction on Build Status migration

--- migrations/20250321133555_update_build_status.js
+++ migrations/20250321133555_update_build_status.js
@@ -11,7 +11,7 @@ exports.up = async function (knex) {
                     'Created',
                     'Scheduled',
                     'Started',
-                    'Completed'
+                    'Completed',
                     'Simulated',
                     'Approved',
                 );
