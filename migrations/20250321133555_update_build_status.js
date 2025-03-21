8fe56bd265fa9034ec19691d52c9c4134dce10f3
priya-00 <priyasadh@hotmail.com>
2025-03-21 13:45:30 +0000

    updating build status enum values

---
+++ migrations/20250321133555_update_build_status.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.schema.raw(`
+        DO $$ 
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'build_status') THEN
+                CREATE TYPE build_status AS ENUM (
+                    'Created',
+                    'Scheduled',
+                    'Started',
+                    'Completed'
+                    'Simulated',
+                    'Approved',
+                );
+            END IF;
+        END $$;
+
+        ALTER TABLE build ADD COLUMN status_new build_status;
+
+        UPDATE build SET status_new = type::text::build_status;
+
+        ALTER TABLE build DROP COLUMN type;
+
+        ALTER TABLE build RENAME COLUMN status_new TO status;
+    `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    console.log("Warning: PostgreSQL does not support removing ENUM values.");
+};
